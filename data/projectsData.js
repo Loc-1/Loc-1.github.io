ffd21b17acea1801565eeafcc594b189694f9814
Lachlan <mlachlan007@gmail.com>
2023-04-20 19:21:33 +0900

    updating

---
+++ data/projectsData.js
@@ -0,0 +1,15 @@
+const projectsData = [
+  {
+    title: 'Research Project: Networks & Distributed Systems',
+    description: `Research project done in the last semester of my Undergraduate degree, following the measurement of network traffic of YouTube Music and Spotifiy in the context of a campus network. Made use of a campus capture card and wireshark`,
+    imgSrc: '/static/images/spotify.png',
+    href: '/static/docs/503_Research_Final_Paper.pdf',
+  },
+  {
+    title: 'Group Project: PumpIT',
+    description: `Group project in the last semester of my Undergraduate degree, created a social media application for users wanting to improve their fitness. Project made use of firebase and react.`,
+    imgSrc: '/static/images/pumpit.png',
+    href: 'https://workout-db-b444f.web.app/',
+  },
+]
+export default projectsData
--- pages/projects.js
+++ pages/projects.js
@@ -12,7 +12,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            A collection of various personal Projects
           </p>
         </div>
         <div className="container py-12">
