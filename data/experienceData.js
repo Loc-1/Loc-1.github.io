5c8629d0b69ae8f79c786ea494066b7586c4824f
Lachlan <mlachlan007@gmail.com>
2023-07-04 21:15:09 +0900

    Author info update

--- data/experienceData.js
+++ data/experienceData.js
@@ -1,40 +1,52 @@
 const experienceData = [
-    {
-      title: 'Sports Coach',
-      company: 'Spoglish GYM',
-      location: 'Tokyo, Japan',
-      range: 'Janurary 2023 - present',
-      url: 'https://www.spoglishgym.jp/',
-      text1: 'Coach of English and basic coordanation skills',
-      text2:
-        'Emphasized a positive attitude and mindset with athletes and co-workers',
-      text3:
-        'Effectively discussed feedback and planning of lessons with assistant and lead coaches',
-    },
-    {
-      title: 'Project Coordinator / Data Analyst',
-      company: 'Athennian',
-      location: 'Calgary, Canada',
-      range: 'September 2020 - July 2021',
-      url: 'https://www.athennian.com/',
-      text1:
-        'Assisted data migration teams for the accurate delivery of customer data from legacy software',
-      text2: 'Wrote code in JavaScript, React, SQL',
-      text3: 'Coordinated tasks performed by Operations and Data Migration teams',
-    },
-    {
-      title: 'U12/U10+ Assistant Coach',
-      company: 'Nakiska Alpine Ski Association',
-      location: 'Calgary, Canada',
-      range: 'November 2017 - May 2022',
-      url: 'https://www.skinasa.org/',
-      text1:
-        'Worked in with athletes between the ages of 8-12 to consolidate and refine basic ski skills',
-      text2:
-        'Communicated and planned daily activities with Head and Assistant coaches',
-      text3:
-        'Awarded Development Level Coach of the Year - 2021/2022 Season',
-    },
-  ]
-  
-  export default experienceData
+  {
+    title: 'Research Assistant',
+    company: 'NICT',
+    location: 'Tokyo, Japan',
+    range: 'July 2023 - present',
+    url: 'https://www.nict.go.jp/en/',
+    text1: 'Create tools for countermeasures against cyber-attacks',
+    text2:
+      'Collect and organize data',
+    text3:
+      'Conduct evaluations under the guidance of lead researcher',
+  },
+  {
+    title: 'Sports Coach',
+    company: 'Spoglish GYM',
+    location: 'Tokyo, Japan',
+    range: 'Janurary 2023 - present',
+    url: 'https://www.spoglishgym.jp/',
+    text1: 'Coach of English and basic coordanation skills',
+    text2:
+      'Emphasized a positive attitude and mindset with athletes and co-workers',
+    text3:
+      'Effectively discussed feedback and planning of lessons with assistant and lead coaches',
+  },
+  {
+    title: 'Project Coordinator / Data Analyst',
+    company: 'Athennian',
+    location: 'Calgary, Canada',
+    range: 'September 2020 - July 2021',
+    url: 'https://www.athennian.com/',
+    text1:
+      'Assisted data migration teams for the accurate delivery of customer data from legacy software',
+    text2: 'Wrote code in JavaScript, React, SQL',
+    text3: 'Coordinated tasks performed by Operations and Data Migration teams',
+  },
+  {
+    title: 'U12/U10+ Assistant Coach',
+    company: 'Nakiska Alpine Ski Association',
+    location: 'Calgary, Canada',
+    range: 'November 2017 - May 2022',
+    url: 'https://www.skinasa.org/',
+    text1:
+      'Worked in with athletes between the ages of 8-12 to consolidate and refine basic ski skills',
+    text2:
+      'Communicated and planned daily activities with Head and Assistant coaches',
+    text3:
+      'Awarded Development Level Coach of the Year - 2021/2022 Season',
+  },
+]
+
+export default experienceData
